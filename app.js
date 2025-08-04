495d2674f3ea60130ebbbbb0779184e7da33f6f8
Imran11150 <imranmehemmedeli82@gmail.com>
2025-08-04 21:30:14 +0400

    html

---
+++ app.js
@@ -0,0 +1,14 @@
+let anakutu = document.getElementById("anakutu")
+
+function sagdansoladuz(){
+anakutu.style.flexDirection = "row"
+
+
+}
+let anakut = document.getElementById("anakutu")
+
+function yuxaridanasagi(){
+anakutu.style.flexDirection = "column"
+
+
+}
